a55efcab9b4d865efd80a8dc843465f1f7c951c8
Michael Leggo <mleggo1@hotmail.com>
2025-11-16 19:55:40 +0800

    Fix GitHub Pages base + rebuild

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Lifespan/',   // 👈 THIS is important
+  base: '/Lifespan/',
+  build: {
+    outDir: 'docs',
+  },
 })
